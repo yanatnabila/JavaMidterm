de363ed77c3af9d218537da5987aaa4e071fb461
= <=>
2021-08-26 22:16:22 -0400

    1st commit

---
+++ src/math/problems/Fibonacci.java
@@ -0,0 +1,17 @@
+package math.problems;
+
+public class Fibonacci {
+
+    /*
+    Write a method that will print or return at least 40 Fibonacci numbers
+
+      0,1,1,2,3,5,8,13
+     */
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
---
+++ src/math/problems/PrimeNumber.java
@@ -0,0 +1,16 @@
+package math.problems;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        /*
+        Write a method to print the list of prime numbers from 2 to 1,000,000
+            Solving the problem should be your first priority, however bonus points if you can figure out how to
+            improve algorithmic efficiency
+
+         Print out the prime numbers in the given range.
+         */
+
+    }
+
+}
