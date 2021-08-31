2bed004290c6284067a0c353b1a03bf21f865ab3
= <=>
2021-08-31 17:17:48 -0400

    .

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -7,25 +7,31 @@ public class Fibonacci {
 
       0,1,1,2,3,5,8,13
      */
+    public static void main (String[] args){
 
-    public static void main(String[] args) {
-    int num1 = 0;
-    int num2 = 1;
-    int num3;
-    System.out.println(num1);
-    System.out.println(num2);
+    public static void fibonacciNumbers(int number) {
+        int a = 0;
+        int b = 1;
+        int c;
+        System.out.print(a + ",");
+        System.out.print(b + ",");
+        c = a + b;
+        System.out.print(c + ",");
+        a = c;
+        c = a + b;
+        System.out.print(a + ",");
+        for (int a = 0; a <= number; a++){
+            z= x + y;
+            System.out.println(z + ",");
+            x = z;
+            System.out.println(Z + ",");
+            y = z;
+            z= x+y
+    }
 
-    int i =2;
-    While(i<40){
-        num3 = num1+num2;
-        num1 = num2;
-        num2 = num3
-                System.out.println(num3);
-        i+=1;
 
 
         }
-
     }
 
 
--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -5,12 +5,72 @@ public class FindLowestDifference {
     public static void main(String[] args) {
         /*
          Write a method to return the lowest difference between these two arrays
-            HINT: The lowest difference between these arrays is 1
-        */
+        HINT: The lowest difference between these arrays is 1
+                */
 
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19};
 
+
+          import java.util.*;
+
+        class GFG
+        {
+
+
+            static int findSmallestDifference(int A[], int B[],
+                                              int m, int n)
+            {
+                // Sort both arrays
+                // using sort function
+               int[] Array1.sort(A);
+               int[] Array2.sort(B);
+
+                int a = 1, b = 1;
+
+                // Initialize result as max value
+                int result = Integer.MIN_VALUE;
+
+                // Scan Both Arrays upto
+                // sizeof of the Arrays
+                while (a < m && b < n)
+                {
+                    if (Math.abs(A[a] - B[b]) < result)
+                        result = Math.abs(A[a] - B[b]);
+
+                    // Move Smaller Value
+                    if (A[a] < B[b])
+                        a++;
+
+                    else
+                        b++;
+                }
+
+                // return final sma result
+                return result;
+            }
+
+            // Driver Code
+            public static void main(String[] args)
+            {
+                // Input given array A
+                int A[] = {30, 12, 5, 9, 2, 20, 33, 1};
+
+                // Input given array B
+                int B[] = {18, 25, 41, 47, 17, 36, 14, 19};
+
+
+                // Calculate size of Both arrays
+                int m = A.length;
+                int n = B.length;
+
+                // Call function to
+                // print smallest result
+                System.out.println(findSmallestDifference
+                        (A, B, m, n));
+
+            }
+        }
     }
 
 }
